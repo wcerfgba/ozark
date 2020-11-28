73237acda4520accf1102fa3f04dcc8e668c7e67
John Preston <wcerfgba@riseup.net>
2020-11-28 20:31:57 +0000

    Start scaffolding app
    * Establish WebSocket connection to Kuzzle
    * Log in to Kuzzle, establishing async messaging pattern
    * Set up React app
    * Set up shadow-cljs

--- ozark-webapp/src/ozark_webapp/core.cljs
+++ ozark-webapp/src/ozark_webapp/core.cljs
@@ -1,9 +1,10 @@
 (ns ozark-webapp.core
-  (:require [clojure.spec.alpha :as s]))
-
-(defn- chan?
-  [c]
-  (= clojure.core.async.impl.channels.ManyToManyChannel (type c)))
+  (:require [clojure.core.async :as async]
+            [clojure.spec.alpha :as s]
+            [goog.object :as o]
+            ["react" :as react]
+            ["react-dom" :as react-dom]
+            ["uuid" :as uuid]))
 
 (s/def :database/document (s/keys :req-un [:database.document/meta]))
 (s/def :database.document/meta (s/keys :opt-un [:database.document/id
@@ -35,7 +36,7 @@
 
 (s/def :database/sub-response (s/and :database/response
                                      (s/keys :opt-un [:database.sub-response/chan])))
-(s/def :database.sub-response/chan chan?)
+(s/def :database.sub-response/chan any?) ;; Should be a channel
 
 (defprotocol Database
   (search [query] "Find documents which match the given query. Returns a 
@@ -49,29 +50,100 @@
                 Returns a sub-response."))
 
 (defn- kuzzle-document->db-document
-  [doc])
+  [doc]
+  nil)
 
 (defn- db-document->kuzzle-document
-  [doc])
+  [doc]
+  nil)
 
 (defn- kuzzle-query->db-query
-  [query])
+  [query]
+  nil)
 
 (defn- db-query->kuzzle-query
-  [query])
+  [query]
+  nil)
 
-(defrecord KuzzleDatabase [config]
+(defrecord ^:private KuzzleDatabase [config]
   Database
   (search
-    [query])
+    [query]
+    nil)
   (put
-    [doc])
+    [doc]
+    nil)
   (delete
-    [doc-id])
+    [doc-id]
+    nil)
   (sub
-    [query]))
+    [query]
+    nil))
+
+(defn- kuzzle-login
+  [username password req-id]
+  #js {"controller" "auth"
+       "action" "login"
+       "strategy" "local"
+       "body" #js {"username" username
+                   "password" password}
+       "requestId" req-id})
+
+(defn- tap-when
+  "Taps a mult and returns a promise channel which will contain the first value 
+   from the mult which matches the predicate."
+  [mult pred]
+  (let [in-ch (async/chan)
+        out-ch (async/promise-chan)]
+    (async/tap mult in-ch)
+    (async/pipeline 1 out-ch (filter pred) in-ch)
+    out-ch))
 
 (defn kuzzle-database
   [config]
-  (let [{:keys [username password endpoint]} config]
-    (->KuzzleDatabase {})))
+  (async/go
+    (let [{:keys [username password endpoint]} config
+          in-ch (async/chan)
+          in-mult (async/mult in-ch)
+          ws (js/WebSocket. endpoint)
+          open-ch (async/chan)]
+      (o/set ws "onopen"
+             (fn [_]
+               (async/go (async/>! open-ch true))))
+      (o/set ws "onerror"
+             (fn [e]
+               (js/console.error e)))
+      (o/set ws "onmessage"
+             (fn [e]
+               (async/go
+                 (js/console.log e)
+                 (async/>! in-ch (js/JSON.parse (o/get e "data"))))))
+      (o/set ws "onclose"
+             (fn [e]
+               (js/console.log e)))
+      (async/<! open-ch)
+      (let [login-req-id (uuid/v4)
+            login-res-ch (tap-when in-mult #(= login-req-id (o/get % "requestId")))
+            _ (.send ws (js/JSON.stringify (kuzzle-login username password login-req-id)))
+            login-res (async/<! login-res-ch)
+            auth (o/get login-res "result")]
+        (js/console.log auth)
+        (->KuzzleDatabase {:ws ws
+                           :in-mult in-mult
+                           :auth auth})))))
+
+;; TODO WebWorker?
+
+(def ^:private e react/createElement)
+
+(defn- app
+  [_props]
+  (async/go
+    (let [db (async/<! (kuzzle-database {:username "john"
+                                         :password "123123"
+                                         :endpoint "ws://localhost:7512"}))]))
+  (e "p" nil "foo"))
+
+(defn- main
+  []
+  (react-dom/render (e app nil nil) (js/document.querySelector "#app")))
